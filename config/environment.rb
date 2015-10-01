0bd8996ed3db7889604ae04ee68adcad5258371b
Luca Spiller <luca@blissfulsystems.com>
2015-10-01 11:09:24 +0200

    Move the Ruote worker to a seperate process to avoid issues caused by ruote-postgres not being threadsafe

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,8 @@ require 'rufus-json'
 require 'ruote-kit'
 require 'ruote-postgres'
 
+# TODO we should seperate this into seperate initialiser files ala Rails
+
 # Load environment from .env in development
 Dotenv.load
 
@@ -11,10 +13,10 @@ Dotenv.load
 Rufus::Json.backend = :yajl
 
 # Connect to postgres and create table if needed
-ruote_storage_connection = PG.connect(ENV["DATABASE_URL"])
-Ruote::Postgres.create_table(ruote_storage_connection)
+$ruote_storage_connection = PG.connect(ENV["DATABASE_URL"])
+Ruote::Postgres.create_table($ruote_storage_connection)
 
 # Setup ruote
 RuoteKit.engine = Ruote::Engine.new(
-    Ruote::Worker.new(
-          Ruote::Postgres::Storage.new(ruote_storage_connection)))
+  Ruote::Postgres::Storage.new($ruote_storage_connection)
+)
